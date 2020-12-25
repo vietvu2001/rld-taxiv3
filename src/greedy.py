1e80b4487048f485414f386738a9a8c4eddbd591
vietvu2001 <theviet.vu@gmail.com>
2020-12-26 02:11:53 +0700

    better version of greedy algorithm

--- src/greedy.py
+++ src/greedy.py
@@ -9,6 +9,7 @@ from taxienv import TaxiEnv
 from qlearn import QAgent
 from termcolor import colored  # pylint: disable=import-error
 from heuristic import cell_frequency, wall_interference, utility
+from connect_qlearn import connected_qlearn
 
 map = [
     "+---------+",
@@ -23,60 +24,121 @@ map = [
 map_to_numpy = np.asarray(map, dtype="c")
 env = TaxiEnv(map_to_numpy)  # reference environment
 
+
 def greedy(env, num_mods):
-    result = []
-    ref_env = copy.deepcopy(env)
-    total = 0
-    for _ in range(num_mods):
-        agent = QAgent(ref_env)
-        agent.qlearn(600, render=False)
-        wall_dict = wall_interference(agent)
-        cell_dict = cell_frequency(agent)
+    # This function returns the sequence of modifications based on the wall and cell heuristics
+    # Parameters
+    # ===============================================================
+    # env: the original environment
+    # num_mods: the number of modifications
+    # ===============================================================
 
-        first = None
-        second = None
-        first_type = None
-        second_type = None
-        modified_1 = None
-        modified_2 = None
+    greedy_seq = []
+    ref = copy.deepcopy(env)
+    agent = None
 
-        if len(wall_dict) != 0 and len(cell_dict) != 0:
-            first = wall_dict[0][0]
-            second = cell_dict[0][0]
-            first_type = 0
-            second_type = 1
-            modified_1 = ref_env.transition([first])
-            modified_2 = copy.deepcopy(ref_env)
-            modified_2.special.append(second)
-
-        elif len(wall_dict) == 0:
-            first = cell_dict[0][0]
-            second = cell_dict[1][0]
-            first_type = 1
-            second_type = 1
-            modified_1 = copy.deepcopy(ref_env)
-            modified_2 = copy.deepcopy(ref_env)
-            modified_1.special.append(first)
-            modified_2.special.append(second)
-
-        agent_1 = QAgent(modified_1)
-        agent_2=  QAgent(modified_2)
-        agent_1.qlearn(600, render=False)
-        agent_2.qlearn(600, render=False)
-
-        u_1 = utility(agent_1)
-        u_2 = utility(agent_2)
-
-        if u_1 >= u_2:
-            ref_env = copy.deepcopy(modified_1)
-            result.append((first_type, first[0], first[1]))
-            total = u_1
+    for i in range(num_mods):
+        # For each iteration, find out the wall that most interferes and the cell that is crossed the most. Try out all options.
+        if i == 0:
+            agent = QAgent(ref)
+            agent.qlearn(600, render=False)
 
         else:
-            ref_env = copy.deepcopy(modified_2)
-            result.append((second_type, second[0], second[1]))
-            total = u_2
-    
-    return (result, total)
+            agent = connected_qlearn(agent, ref, 300)
 
-print(greedy(env, 6))
+        # Take out the lists from the heuristics.
+        wall_dict = wall_interference(agent)
+        cell_dict = cell_frequency(agent)
+        
+
+        # Take out the max values, and the options to try out.
+        wall_nums = [elem[1] for elem in wall_dict]
+        max_wall = max(wall_nums)
+
+        cell_nums = [elem[1] for elem in cell_dict]
+        max_cell = max(cell_nums)
+
+        wall_options = [elem[0] for elem in wall_dict if elem[1] == max_wall]
+        cell_options = [elem[0] for elem in cell_dict if elem[1] == max_cell]
+
+        # Test out all the options, get optimal modification
+        opt_value = float("-inf")
+        opt_choice = None
+        category = -1
+
+        for wall in wall_options:
+            print(colored("Testing environment", "red"))
+            e = ref.transition([wall])
+            new_agent = connected_qlearn(agent, e, 300)
+
+            # Get utility
+            val = utility(new_agent)
+            if val > opt_value:
+                opt_value = val
+                opt_choice = wall
+                category = 0
+
+        for cell in cell_options:
+            print(colored("Testing environment", "red"))
+            e = copy.deepcopy(ref)
+            e.special.append(cell)
+            new_agent = connected_qlearn(agent, e, 300)
+
+            # Get utility
+            val = utility(new_agent)
+            if val > opt_value:
+                opt_value = val
+                opt_choice = cell
+                category = 1
+
+        assert(category != -1)
+
+        # Store found modification and change the reference environment
+        if category == 0:
+            mod = (0, opt_choice[0], opt_choice[1])
+            greedy_seq.append(mod)
+            ref = ref.transition([opt_choice])
+
+        elif category == 1:
+            mod = (1, opt_choice[0], opt_choice[1])
+            greedy_seq.append(mod)
+            ref.special.append(opt_choice)
+
+
+    # Evaluate utility
+    total_agent = QAgent(ref)
+    total_agent.qlearn(600, render=False)
+    result = utility(total_agent)
+    print(colored(result, "red"))
+
+    return greedy_seq, result
+
+
+def greedy_iter(env, num_mods, num_trials):
+    opt_val = float("-inf")
+    opt_seq = None
+
+    for i in range(num_trials):
+        print(colored("Iteration {}".format(i + 1), "red"))
+        seq, val = greedy(env, num_mods)
+        if val > opt_val:
+            opt_val = val
+            opt_seq = seq
+
+    return opt_seq, opt_val
+
+# Test the greedy algorithm
+num_mods = 6
+mod_seq, value = greedy_iter(env, num_mods, 30)
+
+# Store results
+r_dir = os.path.abspath(os.pardir)
+data_dir = os.path.join(r_dir, "data")
+txt_dir = os.path.join(data_dir, "greedy_{}.txt".format(num_mods))
+
+with open(txt_dir, "w") as file:
+    file.write("Modifications: ")
+    file.write(str(mod_seq))
+    file.write("\n")
+    file.write("Utility: ")
+    file.write(str(value))
